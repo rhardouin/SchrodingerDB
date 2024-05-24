6ddcf22e2f67ac283f2b46f0c8d8471145abfadb
github-hugo-vincent <hugo.vincent@teads.tv>
2024-05-24 16:27:59 +0200

    chore: Update Main.java and Transport.java files for server implementation to respond pong to ping

--- src/main/java/schrodingerdb/transport/Transport.java
+++ src/main/java/schrodingerdb/transport/Transport.java
@@ -1,5 +1,35 @@
-package schrodingerdb.transport;
+package main.java.schrodingerdb.transport;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
 
 public class Transport {
-    
+    private Socket socket;
+
+    public Transport(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void handle() {
+        System.out.println("New client connected");
+
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            PrintWriter output = new PrintWriter(this.socket.getOutputStream(), true);
+
+            String receivedText = input.readLine();
+            if ("ping".equalsIgnoreCase(receivedText)) {
+                output.println("pong");
+            } else {
+                output.println("Error: Invalid request");
+            }
+        } catch (SocketException e) {
+            System.out.println("Connection was reset by client");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
