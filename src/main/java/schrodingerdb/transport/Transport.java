c928274037221b307345fdbb0ceb5c599ccf8f58
github-hugo-vincent <hugo.vincent@teads.tv>
2024-05-30 11:41:37 +0200

    COmplete PIng command as REDIS do

--- src/main/java/schrodingerdb/transport/Transport.java
+++ src/main/java/schrodingerdb/transport/Transport.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.io.IOException;
 import java.net.SocketException;
 
 public class Transport {
@@ -17,16 +18,31 @@ public class Transport {
         System.out.println("New client connected");
 
         try(BufferedReader input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));PrintWriter output = new PrintWriter(this.socket.getOutputStream(), true);) {
-            String receivedText = input.readLine();
-            if ("PING".equalsIgnoreCase(receivedText)) {
-                socket.getOutputStream().write("+PONG\r\n".getBytes());
-            } else {
-                output.println("Error: Invalid request");
+            String receivedText;
+            while ((receivedText = input.readLine()) != null) {
+                if (receivedText.startsWith("PING")) {
+                    String[] parts = receivedText.split(" ", 2);
+                    if (parts.length > 1) {
+                        output.println("+"+parts[1]);
+                    } else {
+                        socket.getOutputStream().write("+PONG\r\n".getBytes());
+                    }
+                } else if ("EXIT".equalsIgnoreCase(receivedText)) {
+                    break;
+                } else {
+                    output.println("Error: Invalid request");
+                }
             }
         } catch (SocketException e) {
             System.out.println("Connection was reset by client");
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
+    }
 }
